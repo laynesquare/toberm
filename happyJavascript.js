39904d7de1c83d6b36568050c568ef5b636dcfb4
Laynesquare <cudidioo@gmail.com>
2022-08-05 00:23:06 +0800

    1223

--- happyJavascript.js
+++ happyJavascript.js
@@ -13,9 +13,6 @@ console.log('add from local 1149');
 const no = 'this is from web 1146';
 const no = 'this is from web 1146';
 
-<<<<<<< HEAD
 const no = 'this is from local 1219';
-=======
 
 const yes = 'this is from web 1146';
->>>>>>> c1de6fd9aeb1cecbf09a759cef191d2c4b00be4c
